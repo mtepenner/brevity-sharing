3cef964480b75bdc7f88641ff315d6da53d1e5e6
Matthew Timothy Erwin Penner <mtepenner@gmail.com>
2026-04-03 09:14:27 -0700

    Create timeline.go

---
+++ backend/internal/services/timeline.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	"https://github.com/mtepenner/brevity-sharing/internal/database"
+	"https://github.com/mtepenner/brevity-sharing/internal/models"
+)
+
+// TimelineService isolates the logic for building user feeds
+type TimelineService struct {
+	DB *database.InMemoryDB
+}
+
+func NewTimelineService(db *database.InMemoryDB) *TimelineService {
+	return &TimelineService{DB: db}
+}
+
+// GetHomeFeed abstracts the complexity of fetching tweets.
+// In the future, this is where you would implement caching lookups
+// or fan-out read architecture before ever touching the main database.
+func (s *TimelineService) GetHomeFeed() []models.Tweet {
+	// For the MVP, it simply passes through to the DB
+	return s.DB.GetTimeline()
+}
