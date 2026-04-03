e4ce09bfa13f951d173833b2b3fa6add723c0e17
Matthew Timothy Erwin Penner <mtepenner@gmail.com>
2026-04-03 09:21:42 -0700

    Update main.go

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -19,8 +19,8 @@ func main() {
 
 	// Define our routes (Requires Go 1.22+ for method-based routing)
 	mux := http.NewServeMux()
-	mux.HandleFunc("POST /api/tweets", srv.HandlePostTweet)
-	mux.HandleFunc("GET /api/timeline", srv.HandleGetTimeline)
+	mux.HandleFunc("POST /api/tweets", api.EnableCORS(srv.HandlePostTweet))
+	mux.HandleFunc("GET /api/timeline", api.EnableCORS(srv.HandleGetTimeline))
 
 	// Start the server
 	port := ":8080"
